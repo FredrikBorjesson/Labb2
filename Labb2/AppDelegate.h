46cfa80331580a87d31fe1c23bee485ce6c06800
fredrikborjesson <fredrik.m.borjesson@gmail.com>
2017-01-17 09:11:09 +0100

    Initial Commit

---
+++ Labb2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb2
+//
+//  Created by Fredrik Börjesson on 17/01/17.
+//  Copyright © 2017 Fredrik Börjesson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Labb2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb2
+//
+//  Created by Fredrik Börjesson on 17/01/17.
+//  Copyright © 2017 Fredrik Börjesson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
